1db9cce281ffc45b2f8e8b7e901198e5ffaa3130
Jordan Farkas <jordfark@gmail.com>
2018-08-08 02:18:50 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 # name_hash = {:blake => 500, :ashley => 2, :adam => 1}
 
 def key_for_min_value(name_hash)
-  lowest_value = 0
+  lowest_value = Float::INFINITY
   lowest_key = nil 
   name_hash.each do |name, age|
     if age <= lowest_value 
